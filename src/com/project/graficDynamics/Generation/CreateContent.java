986abeab6a411310a6572fbcc78eb94fc943dd1d
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-06 01:56:29 -0300

    Criando estrutura inicial

---
+++ src/com/project/graficDynamics/Generation/CreateContent.java
@@ -0,0 +1,9 @@
+package com.project.graficDynamics.Generation;
+
+import javafx.scene.Parent;
+
+public class CreateContent {
+   
+	public Parent createContent() {
+ 	}
+}
--- src/com/project/graficDynamics/MainGraficDynam.java
+++ src/com/project/graficDynamics/MainGraficDynam.java
@@ -1,15 +1,20 @@
 package com.project.graficDynamics;
 
+import com.project.graficDynamics.Generation.CreateContent;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainGraficDynam extends Application{
 
 	@Override
-	public void start(Stage arg0) throws Exception {
- 		
+	public void start(Stage stage) throws Exception {
+	   CreateContent create = new CreateContent();
+	   
+       stage.setScene(new Scene(create.createContent())); 		
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
