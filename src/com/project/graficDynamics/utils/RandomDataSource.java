7e91f8f838f5ebf8f0b7d13efc86e34e00753a4b
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-06 02:17:22 -0300

    implementando classe com valores randomicos

---
+++ src/com/project/graficDynamics/utils/RandomDataSource.java
@@ -0,0 +1,13 @@
+package com.project.graficDynamics.utils;
+
+import com.project.graficDynamics.DataSource.DataSource;
+
+class RandomDataSource implements DataSource<Double> {
+
+	@Override
+	public Double getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
