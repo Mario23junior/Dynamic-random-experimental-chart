26a8ceb21c97fcee1093c793850b1095d5b7b4ec
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-06 01:44:15 -0300

    Grafico dynamico em tempo real

---
+++ src/com/project/graficDynamics/MainGraficDynam.java
@@ -0,0 +1,17 @@
+package com.project.graficDynamics;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainGraficDynam extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+ 		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
